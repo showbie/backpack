8b2d4bbb18538be90e7704e5916e2626902040fc
Jeff <rustyangel@gmail.com>
2020-02-04 09:52:20 -0700

    Add variables to template context

--- packages/generator-backpack/src/react/index.ts
+++ packages/generator-backpack/src/react/index.ts
@@ -3,7 +3,15 @@ import * as path from 'path';
 
 import Generator = require('yeoman-generator');
 
+type Fields = {
+  componentName: string;
+};
+
 export default class ReactComponentGenerator extends Generator {
+  public fields: Fields = {
+    componentName: '',
+  };
+
   constructor(args: any, opts: any) {
     super(args, opts);
 
@@ -13,16 +21,18 @@ export default class ReactComponentGenerator extends Generator {
 
   // public async prompting() {}
 
-  public writing() {
+  public writing(): void {
     let templateDir = `${__dirname}/templates`;
     let { componentName, path: destPath } = this.options;
+
+    this.fields.componentName = componentName;
 
     fs.readdirSync(templateDir).forEach((file) => {
       this.fs.copyTpl(
         // util.format()
         this.templatePath(path.join(templateDir, file)),
         this.destinationPath(path.join(destPath, componentName, file)),
-        {}
+        this.fields
       );
     });
   }
