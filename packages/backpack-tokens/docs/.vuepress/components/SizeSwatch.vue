9da1bd25fb8253b0236fd21710696a43820cc757
Jeff <rustyangel@gmail.com>
2020-02-10 13:19:50 -0700

    Add typography example components

---
+++ packages/backpack-tokens/docs/.vuepress/components/SizeSwatch.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="size flex mt4 mb4">
+    <dl class="w3 ma0">
+      <dt class="f7 fw6 lh-copy ttu">{{ name }}</dt>
+      <dd class="f5 ml0">
+        <code>{{ fontSize }}</code>
+      </dd>
+    </dl>
+    <div class="ml3" :style="`font-size: ${fontSize}`">
+      The quick brown fox jumps over the lazy dog.
+    </div>
+  </div>
+</template>
+
+<script>
+const ROOT_SIZE = 12;
+
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+  },
+
+  computed: {
+    fontSize() {
+      let val = /([0-9.]+)/.exec(this.value)[1];
+      return `${Math.floor(val * ROOT_SIZE)}px`;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ packages/backpack-tokens/docs/.vuepress/components/WeightSwatch.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="size flex mt4 mb4">
+    <dl class="w3 ma0">
+      <dt class="f7 fw6 lh-copy ttu">{{ weight }}</dt>
+      <dd class="f5 ml0">
+        <code>{{ name }}</code>
+      </dd>
+    </dl>
+    <div class="ml3" :style="`font-size: 18px; font-weight: ${weight}`">
+      The quick brown fox jumps over the lazy dog.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+    weight: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style></style>
