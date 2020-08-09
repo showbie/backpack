a707ed2a82e8524698ce55bf9866bf97e50c0adf
Jeff <rustyangel@gmail.com>
2020-08-09 14:49:25 -0600

    Include tsx files in linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,19 @@ module.exports = {
   rules: {},
   overrides: [
     {
-      files: ['packages/**/*.ts'],
+      files: ['packages/**/*.{ts,tsx}'],
       extends: [
         '@showbie/eslint-config/react',
         'plugin:react-hooks/recommended',
       ],
       env: {
         node: true,
+      },
+      globals: {
+        __DEV__: 'readonly',
+      },
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
       },
     },
   ],
