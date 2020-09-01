23125a474eb1c3b83869ba6e654cd67254cf2c32
Jeff <rustyangel@gmail.com>
2020-09-01 15:35:26 -0600

    Remove temp components

--- packages/backpack-react/src/components/Text/TextContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-import { UseTextProps } from '../../hooks/typography';
-
-const textContext = createContext<UseTextProps | false>(false);
-
-export default textContext;
