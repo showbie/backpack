b0a30d867e9126168c6cb2d7a4b7f323fc3a2c49
Jeff <rustyangel@gmail.com>
2020-10-04 23:42:33 -0600

    Add tests

--- packages/backpack-react/src/components/Inline/Inline.test.tsx
+++ packages/backpack-react/src/components/Inline/Inline.test.tsx
@@ -5,14 +5,30 @@ import { render } from '@testing-library/react';
 import { Inline } from './Inline';
 
 describe('<Inline />', () => {
-  it('renders', () => {
-    let { getByText } = render(
+  it('renders a div by default', () => {
+    let { queryAllByRole, getByText } = render(
       <Inline>
         <div>Label</div>
       </Inline>
     );
-    let element = getByText('Label');
 
-    expect(element).toBeInTheDocument();
+    expect(getByText('Label')).toBeInTheDocument();
+    expect(queryAllByRole('list').length).toBe(0);
+    expect(queryAllByRole('listitem').length).toBe(0);
+  });
+
+  it('renders `li` elements within a list parent', () => {
+    let { getByRole } = render(
+      <Inline tagName="ul">
+        <div>1</div>
+        <div>2</div>
+      </Inline>
+    );
+    let list = getByRole('list');
+
+    expect(list.nodeName).toBe('UL');
+    expect(
+      Array.from(list.childNodes).map((childNode) => childNode.nodeName)
+    ).toEqual(['LI', 'LI']);
   });
 });
--- packages/backpack-react/src/components/Stack/Stack.test.tsx
+++ packages/backpack-react/src/components/Stack/Stack.test.tsx
@@ -5,14 +5,30 @@ import { render } from '@testing-library/react';
 import { Stack } from './Stack';
 
 describe('<Stack />', () => {
-  it('renders', () => {
-    let { getByText } = render(
+  it('renders a div by default', () => {
+    let { queryAllByRole, getByText } = render(
       <Stack>
         <div>Label</div>
       </Stack>
     );
-    let element = getByText('Label');
 
-    expect(element).toBeInTheDocument();
+    expect(getByText('Label')).toBeInTheDocument();
+    expect(queryAllByRole('list').length).toBe(0);
+    expect(queryAllByRole('listitem').length).toBe(0);
+  });
+
+  it('renders `li` elements within a list parent', () => {
+    let { getByRole } = render(
+      <Stack tagName="ul">
+        <div>1</div>
+        <div>2</div>
+      </Stack>
+    );
+    let list = getByRole('list');
+
+    expect(list.nodeName).toBe('UL');
+    expect(
+      Array.from(list.childNodes).map((childNode) => childNode.nodeName)
+    ).toEqual(['LI', 'LI']);
   });
 });
