0dacc81acfc2d5e65a695d75e5808f9f6c3572d9
Jeff <rustyangel@gmail.com>
2020-12-09 12:32:33 -0700

    Ensure status output appears in proper order

--- packages/stickerbomb/src/cli.ts
+++ packages/stickerbomb/src/cli.ts
@@ -8,14 +8,42 @@ import { writeIndex } from './writeIndex';
 sade('stickerbomb [src] [dest]', true)
   .version('1.0.0')
   .describe('Generate icon components from SVG artwork')
-  .action((src, dest, opts) => {
+  .action(async (src, dest, opts) => {
     let filesIn = src || 'icons/*.svg';
     let filesOut = dest || 'components/icons';
 
-    console.log(filesIn, filesOut, opts);
+    // console.log(filesIn, filesOut, opts);
 
-    ora.promise(removeExisting(filesOut), 'Removing existing components...');
-    ora.promise(generateIcons(filesIn, filesOut), 'Reticulating splines...');
-    ora.promise(writeIndex(filesOut), 'Writing component index...');
+    let spinner = ora().start();
+
+    try {
+      spinner.start('Removing existing components...');
+
+      await removeExisting(filesOut);
+
+      spinner.succeed('Removing existing components... DONE');
+    } catch (error) {
+      spinner.fail('Removing existing components... FAILED')
+    }
+
+    try {
+      spinner.start('Reticulating splines...')
+
+      await generateIcons(filesIn, filesOut);
+
+      spinner.succeed('Reticulating splines... DONE');
+    } catch (error) {
+      spinner.fail('Reticulating splines... FAILED')
+    }
+
+    try {
+      spinner.start('Writing component index...')
+
+      await writeIndex(filesOut)
+
+      spinner.succeed('Writing component index... DONE');
+    } catch (error) {
+      spinner.fail('Writing component index... FAILED')
+    }
   })
   .parse(process.argv);
