d59c03e64e64ebe85e357502c3bac267e4589dae
Jeff <rustyangel@gmail.com>
2020-12-18 09:25:43 -0700

    Add dynamic title statement to Ember svg components

--- packages/stickerbomb/src/svgToHtmlbars.ts
+++ packages/stickerbomb/src/svgToHtmlbars.ts
@@ -4,19 +4,31 @@ import toXml from 'xast-util-to-xml';
 
 export async function svge(svg: string): Promise<string> {
   let hast = parse(svg);
-  // console.log(svgHAST);
-  // {
-  //   type: 'root',
-  //   children: [
-  //     {
-  //       type: 'element',
-  //       tagName: 'svg',
-  //       properties: [Object],
-  //       children: [Array],
-  //       metadata: ''
-  //     }
-  //   ]
-  // }
+
+  /**
+   * Compiles to
+   * {{#if @title}}<title id={{@titleId}}>{{@title}}</title>{{/if}}
+   */
+  let dynamicTitle = [
+    { type: 'text', value: '\n{{#if @title}}' },
+    {
+      type: 'element',
+      tagName: 'title',
+      properties: { id: '{{@titleId}}' },
+      children: [{ type: 'text', value: '{{@title}}' }],
+    },
+    { type: 'text', value: '{{/if}}\n' },
+  ];
+
+  /**
+   * 1. Check if <title> node aready exists
+   * 2. Remove existing element
+   * 3. Add dynamic title node
+   * 4. Add ...attributes to root svg node
+   */
+  // @ts-expect-error -- Typing doesn't expect children to exist on root node.
+  hast.children[0].children.unshift(...dynamicTitle);
+
   let xast = toXast(hast);
   let newSvg = toXml(xast);
 
